68d9eb1a0491da6cbc7cd7b213610232efa7eae6
Rushikesh Sunil Khare <rushikesh.khare29@gmail.com>
2023-04-24 23:27:50 +0530

    hello

--- router.js
+++ router.js
@@ -3,11 +3,16 @@ import { printDate, printMonth, printBatchInfo } from './util/helper.js';
 import { trimString, changetoLowerCase, changeToUpperCase } from './validator/formatter.js';
 import { chunkMonths, lastNine, unionArr, movieObj } from './demo.js';
 import express from 'express'
+// import { chunk } from 'lodash';
 const app = express()
 const port = 3000;
 
 app.get('/test-me', (req, res) => {
-  res.send(chunkMonths() + lastNine() + unionArr() + movieObj());
+  const chunk1 = chunkMonths();
+  const lastnine = lastNine();
+  const unionarr = unionArr();
+  const movieobj = movieObj();
+  res.json({lastnine, chunk1, unionarr, movieobj});
 })
 
 
